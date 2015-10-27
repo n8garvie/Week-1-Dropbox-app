e0a9d57c74a1412b0f5c9264288c3358c2a74dcf
Nathan Garvie <nathang87@gmail.com>
2015-10-26 22:19:39 -0700

    Fixed terms of service dismiss and added gif

---
+++ dropbox/TermsOfServiceViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TermsOfServiceViewController.swift
+//  dropbox
+//
+//  Created by Nathan Garvie on 10/26/15.
+//  Copyright © 2015 Nathan. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTapDone(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
